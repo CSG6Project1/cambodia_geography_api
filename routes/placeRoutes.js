ad6bc6fed38ec4c9ae7e8bff031412b24c129a89
Sopanha Kok <sopanhakok@gmail.com>
2021-07-20 18:53:51 +0700

    feat: add get place by id

--- routes/placeRoutes.js
+++ routes/placeRoutes.js
@@ -3,11 +3,12 @@ import paginatedResult from '../middlewares/placeMiddleware.js'
 import commentResult from '../middlewares/commentMiddleware.js'
 import Place from '../models/placeModels.js'
 import Comment from '../models/commentModels.js'
-import { getPlaces } from '../controllers/placeController.js'
+import { getPlaces, getPlaceDetail } from '../controllers/placeController.js'
 import { getComments } from '../controllers/commentController.js'
 const router = express.Router()
 
 router.route('/').get(paginatedResult(Place, 'comments'), getPlaces)
+router.route('/:id').get(getPlaceDetail)
 router.route('/:id/comments').get(commentResult(Place, 'comments'), getComments)
 
 export default router
