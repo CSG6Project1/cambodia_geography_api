84dee0ea6656f5b18d344d076005ba1db6705ac7
Sopanha Kok <sopanhakok@gmail.com>
2021-07-23 21:08:47 +0700

    close #21 List All User API

--- controllers/userController.js
+++ controllers/userController.js
@@ -62,4 +62,152 @@ const userRegister = asyncHandler(async (req, res) => {
   }
 })
 
-export { userLogin, userRegister }
+const linkPaginate = (increase, decrease, listQuery, first, last) => {
+  let base_URL = `${process.env.HOST}/user/all`
+  let sign = '?'
+  listQuery.forEach((q, index) => {
+    if (index !== 0) sign = '&'
+    if (increase && q.name === 'page') {
+      base_URL += `${sign}${q.name}=${q.value + 1}`
+    } else if (first && q.name === 'page') {
+      base_URL += `${sign}${q.name}=1`
+    } else if (last && q.name === 'page') {
+      base_URL += `${sign}${q.name}=${last}`
+    } else if (decrease && q.name === 'page') {
+      base_URL += `${sign}${q.name}=${q.value - 1}`
+    } else {
+      base_URL += `${sign}${q.name}=${q.value}`
+    }
+  })
+  return base_URL
+}
+
+const userList = (model) =>
+  asyncHandler(async (req, res) => {
+    const page = parseInt(req.query.page) || 1
+    const limit = parseInt(req.query.per_page) || 5
+    const filter = req.query.filter
+    const listQuery = []
+    let modelLength = ''
+    const role = req.role
+    if (role !== 'admin') {
+      res.send({
+        message: 'User is not allowed',
+      })
+      return
+    }
+
+    if (filter) {
+      modelLength = await model.countDocuments({ role: filter }).exec()
+      listQuery.push(
+        { name: 'filter', value: filter },
+        { name: 'page', value: page }
+      )
+    } else {
+      modelLength = await model.countDocuments().exec()
+      listQuery.push({ name: 'page', value: page })
+    }
+
+    if (req.query.per_page) {
+      listQuery.push({ name: 'per_page', value: limit })
+    }
+
+    const startIndex = (page - 1) * limit
+    const endIndex = page * limit
+
+    let links = {}
+    let data = []
+    let meta = {}
+
+    meta.count = limit
+    meta.total_count = modelLength
+    meta.total_pages = Math.ceil(modelLength / limit)
+
+    if (endIndex === modelLength) {
+      links.self = linkPaginate(false, false, listQuery, false, false)
+      links.first = linkPaginate(false, false, listQuery, true, false)
+      links.last = linkPaginate(
+        false,
+        false,
+        listQuery,
+        false,
+        Math.ceil(modelLength / limit)
+      )
+    }
+
+    if (endIndex < modelLength) {
+      links.self = linkPaginate(false, false, listQuery, false, false)
+      if (page + 1 <= Math.ceil(modelLength / limit)) {
+        links.next = linkPaginate(true, false, listQuery, false, false)
+      }
+
+      if (page - 1 > 0) {
+        links.prev = linkPaginate(false, true, listQuery, false, false)
+      }
+
+      links.first = linkPaginate(false, false, listQuery, true, false)
+      links.last = linkPaginate(
+        false,
+        false,
+        listQuery,
+        false,
+        Math.ceil(modelLength / limit)
+      )
+    } else if (endIndex > modelLength) {
+      links.self = linkPaginate(false, false, listQuery, false, false)
+      links.first = linkPaginate(false, false, listQuery, true, false)
+      links.last = linkPaginate(
+        false,
+        false,
+        listQuery,
+        false,
+        Math.ceil(modelLength / limit)
+      )
+    }
+
+    if (startIndex > 0) {
+      links.self = linkPaginate(false, false, listQuery, false, false)
+      if (page + 1 < Math.ceil(modelLength / limit)) {
+        links.next = linkPaginate(true, false, listQuery, false, false)
+      }
+
+      if (page - 1 > 0) {
+        links.prev = linkPaginate(false, true, listQuery, false, false)
+      }
+      links.first = linkPaginate(false, false, listQuery, true, false)
+      links.last = linkPaginate(
+        false,
+        false,
+        listQuery,
+        false,
+        Math.ceil(modelLength / limit)
+      )
+    }
+    try {
+      if (filter) {
+        data = await User.find({ role: filter })
+          .select('-password -email -credential_id')
+          .limit(limit)
+          .skip(startIndex)
+          .exec()
+      } else {
+        data = await User.find()
+          .select('-password -email -credential_id')
+          .limit(limit)
+          .skip(startIndex)
+          .exec()
+      }
+    } catch (error) {
+      res.status(500).json({ message: error.message })
+    }
+
+    const response = {
+      data: data,
+      meta,
+      links,
+    }
+    res.status(200)
+    res.send(response)
+  })
+
+export { userLogin, userRegister, userList }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,20 @@
 import express from 'express'
-import { userLogin, userRegister } from '../controllers/userController.js'
+import {
+  userLogin,
+  userRegister,
+  userList,
+} from '../controllers/userController.js'
+import authMiddleware from '../middlewares/authMiddleware.js'
 import {
   userEmail,
   userCredential,
   userRefreshToken,
 } from '../middlewares/userMiddleware.js'
+import User from '../models/userModels.js'
 
 const router = express.Router()
 
+router.route('/all').get(authMiddleware, userList(User))
 router
   .route('/token')
   .post(userEmail, userCredential, userRefreshToken, userLogin)
