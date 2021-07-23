2db546d6c60690eab1181a4f264dfc4d8e4f6318
Sopanha Kok <sopanhakok@gmail.com>
2021-07-23 21:38:57 +0700

    close #22 User Detail API

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import connectDB from './config/db.js'
 import placeRoutes from './routes/placeRoutes.js'
 import commentRoutes from './routes/commentRoutes.js'
 import userRoutes from './routes/userRoutes.js'
+import authMiddleware from './middlewares/authMiddleware.js'
+import { userDetail } from './controllers/userController.js'
 
 dotenv.config()
 connectDB()
@@ -13,10 +15,11 @@ app.use(express.json())
 app.get('/', (req, res) => {
   res.send('API is running....')
 })
-
+app.get('/account', authMiddleware, userDetail)
 app.use('/places', placeRoutes)
 app.use('/comment', commentRoutes)
 app.use('/user', userRoutes)
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
