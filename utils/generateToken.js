e7067b7568a336b20fc1af076fe54a60be96e425
Sopanha Kok <sopanhakok@gmail.com>
2021-07-31 22:31:38 +0700

    refactor: update parameter utils

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -6,8 +6,8 @@ const generateToken = (id, role) => {
   return jwt.sign({ id, role }, process.env.JWT_SECRET, { expiresIn: '2h' })
 }
 
-const generateRefreshToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_REFRESH_SECRET)
+const generateRefreshToken = (id, role) => {
+  return jwt.sign({ id, role }, process.env.JWT_REFRESH_SECRET)
 }
 
 export { generateToken, generateRefreshToken }
