6c1992f8a543e43729bb5b01a1e294ad8258b1d6
Rotsarak Kang <63399044+Rotsarak@users.noreply.github.com>
2021-08-03 16:42:04 +0700

    close #36 divide search api to autocompleter api
    Co-authored-by: Jayden-ka <63399044+Jayden-ka@users.noreply.github.com>

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import connectDB from './config/db.js'
 import placeRoutes from './routes/placeRoutes.js'
 import searchplaceRoutes from './routes/searchplaceRoutes.js'
 import filterplaceRoutes from './routes/filterplaceRoutes.js'
+import autocompleterRoutes from './routes/autocompleterRoutes.js'
 import commentRoutes from './routes/commentRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import authMiddleware from './middlewares/authMiddleware.js'
@@ -23,6 +24,7 @@ app.get('/account', authMiddleware, userDetail)
 app.use('/places', placeRoutes)
 app.use('/searchplaces', searchplaceRoutes)
 app.use('/filterplaces', filterplaceRoutes)
+app.use('/autocompleter', autocompleterRoutes)
 app.use('/comment', commentRoutes)
 app.use('/user', userRoutes)
 
