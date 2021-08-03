8112cb97e28fde2470c47a0cbcc7448bf7cd7b10
Jayden-ka <63399044+Jayden-ka@users.noreply.github.com>
2021-08-03 23:45:08 +0700

    close #40 paginate filter place api

--- routes/searchplaceRoutes.js
+++ routes/searchplaceRoutes.js
@@ -1,17 +1,11 @@
 import express from 'express'
 import fuzzysearch from 'fuzzy-search'
 import Place from '../models/placeModels.js'
-import {
-    getPlaces,
-  } from '../controllers/searchplaceControllers.js'
+import { getSearchPlaces } from '../controllers/searchplaceControllers.js'
 const router = express.Router()
 
 ///searchplaces/result?keyword=
 
-router.get("/result",getPlaces(Place));
+router.get('/result', getSearchPlaces(Place))
 
 export default router
-
-
-
-
